1ab78604fa94356a44102f3896e64e1682a3d803
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2024-11-27 13:53:34 +0800

    chore: 添加pinia-plugin-persistedstate

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,15 @@ import "@/assets/base.css";
 import "@/assets/theme.css";
 import ArcoVue from '@arco-design/web-vue';
 import '@arco-design/web-vue/dist/arco.css';
+import piniaPersist from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
-
+const pinia =createPinia()
+pinia.use(piniaPersist)
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(ArcoVue)
 
